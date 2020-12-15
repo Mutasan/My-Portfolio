1792a238a680f556f467d4b0605d9b5e98b88d72
Mutasan <clarinette.1030@gmail.com>
2020-12-15 23:59:40 +0900

    add some page, no contents yet

---
+++ components/molecules/HeaderButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul>
+      <li>Profile</li>
+      <li>Posts</li>
+      <li>Career</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeaderButton extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
